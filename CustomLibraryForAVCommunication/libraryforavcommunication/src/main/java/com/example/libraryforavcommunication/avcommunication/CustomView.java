58b4bea9e0824057f3633dec8132f27d5fa725aa
komal29niyaan <komalpardeshi3216@gmail.com>
2019-06-19 17:02:00 +0530

    added attributeset to customview class

--- CustomLibraryForAVCommunication/libraryforavcommunication/src/main/java/com/example/libraryforavcommunication/avcommunication/CustomView.java
+++ CustomLibraryForAVCommunication/libraryforavcommunication/src/main/java/com/example/libraryforavcommunication/avcommunication/CustomView.java
@@ -7,13 +7,18 @@ import android.util.AttributeSet;
 import com.google.android.exoplayer2.ui.PlayerView;
 
 public class CustomView extends PlayerView {
-    public CustomView(Context context) {
+  /*  public CustomView(Context context) {
         super(context);
     }
+    */
+/*TODO: Unable to start activity ComponentInfo{com.example.videomodule/com.example.videomodule.VideoModuleActivity}
+  Error inflating class com.example.libraryforavcommunication.avcommunication.CustomView
+  Android expects an AttributeSet for the internal LayoutInflater.So just add the AttributeSet to your CustomView Constructor
+  */
 
-   /* public CustomView(Context context, AttributeSet attrs) {
+  public CustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
-*/
+
 
 }
